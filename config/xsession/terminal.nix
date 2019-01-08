b9f0d9de93b4c238c7faff6defbb897a1c6fa1e9
Daniel Løvbrøtte Olsen <daniel@dodsorf.as>
2019-01-09 00:29:11 +0100

    Remove old urxvt configs

--- config/xsession/terminal.nix
+++ config/xsession/terminal.nix
@@ -3,36 +3,6 @@
 
   imports = [ ./zsh/zsh.nix ];
 
-#  programs.urxvt = {
-#    enable = true;
-#    fonts = [ "xft:DejaVu Sans Mono Nerd Font:size=12" ];
-#    scroll.bar.enable = false;
-#    shading = 20;
-#    extraConfig = {
-#      "foreground" = "#cccccc";
-#      "tintColor" = "white";
-#      "depth" = 32;
-#      "background" = "rgba:0000/0000/0200/c800";
-#      "color0" = "#000000";
-#      "color1" = "#9e1828";
-#      "color2" = "#aece92";
-#      "color3" = "#968a38";
-#      "color4" = "#414171";
-#      "color5" = "#963c59";
-#      "color6" = "#418179";
-#      "color7" = "#bebebe";
-#      "color8" = "#666666";
-#      "color9" = "#cf6171";
-#      "color10" = "#c5f779";
-#      "color11" = "#fff796";
-#      "color12" = "#4186be";
-#      "color13" = "#cf9ebe";
-#      "color14" = "#71bebe";
-#      "color15" = "#ffffff";
-#      "buffered" = "false";
-#    };
-#  };
-
 
   home.file.kitty = {
     target = ".config/kitty/kitty.conf";
