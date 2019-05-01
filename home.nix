5a22d677e7d4352b402d6e09aa4d40f56ae11da1
Daniel Løvbrøtte Olsen <daniel@dodsorf.as>
2019-05-01 23:59:21 +0200

    19.03

--- home.nix
+++ home.nix
@@ -99,6 +99,6 @@
 
   programs.home-manager = {
     enable = true;
-    path = "https://github.com/rycee/home-manager/archive/master.tar.gz";
+    path = "https://github.com/rycee/home-manager/archive/release-19.03.tar.gz";
   };
 }
