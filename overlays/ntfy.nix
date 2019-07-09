e8cdba0b0136665f9fc2a0e7a41205a124d436a9
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2019-07-09 01:12:12 +0000

    Update ntfy.nix

--- overlays/ntfy.nix
+++ overlays/ntfy.nix
@@ -5,8 +5,8 @@ self: super:
       ntfy = python-super.ntfy.overrideAttrs (oldAttrs: {
         src = super.fetchgit {
           url = "https://github.com/dschep/ntfy";
-          rev = "1f6721cb6e41d3fd0de6ddb7a4f1302551e1c783";
-          sha256 = "1n08w6h1narq3jwhd4k1p23hyysn6knaij0r1gpnakx9437h6yk4";
+          rev = "c16526bbaf5149db6bc4ef12f2f2c06c114cce45";
+          sha256 = "1myj6krccnnxhs6lgl42199ygcaxmmrqp1lqrvn8i1ypm3qryc9b";
         };
         propagatedBuildInputs = with python-self; [ requests ruamel_yaml appdirs mock sleekxmpp emoji psutil dbus-python matrix-client ];
         preBuild = ''
