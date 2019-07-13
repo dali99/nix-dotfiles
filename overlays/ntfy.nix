d10b68c4ecedf30d003e117730073027c7d4f0ab
Daniel Løvbrøtte Olsen <daniel@dodsorf.as>
2019-07-13 03:01:05 +0200

    fix ntfy overlay

--- overlays/ntfy.nix
+++ overlays/ntfy.nix
@@ -1,18 +1,11 @@
 self: super:
 {
-  python = super.python.override {
-    packageOverrides = python-self: python-super: {
-      ntfy = python-super.ntfy.overrideAttrs (oldAttrs: {
-        src = super.fetchgit {
-          url = "https://github.com/dschep/ntfy";
-          rev = "c16526bbaf5149db6bc4ef12f2f2c06c114cce45";
-          sha256 = "1myj6krccnnxhs6lgl42199ygcaxmmrqp1lqrvn8i1ypm3qryc9b";
-        };
-        propagatedBuildInputs = with python-self; [ requests ruamel_yaml appdirs mock sleekxmpp emoji psutil dbus-python matrix-client ];
-        preBuild = ''
-          export HOME="$TMP"
-        '';
-      });
+  ntfy = super.ntfy.overrideAttrs (old: rec {
+    src = super.fetchFromGitHub {
+      owner = "dschep";
+      repo = "ntfy";
+      rev = "d7a359bd6e4902e0067c62058179d8c678361154";
+      sha256 = "1n08w6h1narq3jwhd4k1p23hyysn6knaij0r1gpnakx9437h6yk4";
     };
-  };
+  });
 }
