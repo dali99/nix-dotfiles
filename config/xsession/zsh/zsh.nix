27f1964e3d86b7e94a41781b684f4b3983718c9a
Daniel Løvbrøtte Olsen <daniel.olsen99@gmail.com>
2020-02-26 08:50:29 +0000

    Move powerlevel9K variables to sessionVariables
    also fix indentation

--- config/xsession/zsh/zsh.nix
+++ config/xsession/zsh/zsh.nix
@@ -18,43 +18,42 @@
       ];
       theme = "powerlevel9k/powerlevel9k";
     };
+    sessionVariables = {
+        POWERLEVEL9K_MODE = "nerdfont-complete";
+        POWERLEVEL9K_LEFT_PROMPT_ELEMENTS = "(os_icon context dir newline vcs)";
+        POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
+        
+        POWERLEVEL9K_OS_ICON_BACKGROUND="white"
+        POWERLEVEL9K_OS_ICON_FOREGROUND="blue"
+        POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="green"
+        POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND="white"
+        POWERLEVEL9K_CONTEXT_SUDO_BACKGROUND="red"
+        POWERLEVEL9K_CONTEXT_SUDO_FOREGROUND="white"
+        POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
+        POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
+        POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
+        
+        POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+        POWERLEVEL9K_SHORTEN_DELIMITER=".."
+        
+        POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="↱"
+        POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="↳ "
+    }
     initExtra = ''
-POWERLEVEL9K_MODE='nerdfont-complete'
+      export MATRIXDEV_HOMESERVER="https://matrix.dodsorf.as"
+      export ***REMOVED***
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir newline vcs)
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status time)
+      export FV_KUBECONFIG="$HOME/.kube/config-fv"
+      export FV_KUBECONFIG="$HOME/.kube/config-fv-prod"
 
-POWERLEVEL9K_OS_ICON_BACKGROUND="white"
-POWERLEVEL9K_OS_ICON_FOREGROUND="blue"
+      export FV_ARM_SUBSCRIPTION_ID="***REMOVED***"
+      export FV_ARM_CLIENT_ID="***REMOVED***"
+      export FV_ARM_CLIENT_SECRET="***REMOVED***"
+      export FV_ARM_TENANT_ID="***REMOVED***"
+      export FV_ARM_ENVIRONMENT="public"
 
-POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND="green"
-POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND="white"
-POWERLEVEL9K_CONTEXT_SUDO_BACKGROUND="red"
-POWERLEVEL9K_CONTEXT_SUDO_FOREGROUND="white"
-
-POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
-POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
-POWERLEVEL9K_SHORTEN_DELIMITER=".."
-
-POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="↱"
-POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="↳ "
-
-export MATRIXDEV_HOMESERVER="https://matrix.dodsorf.as"
-export ***REMOVED***
-
-export FV_KUBECONFIG="$HOME/.kube/config-fv"
-export FV_KUBECONFIG="$HOME/.kube/config-fv-prod"
-
-export FV_ARM_SUBSCRIPTION_ID="***REMOVED***"
-export FV_ARM_CLIENT_ID="***REMOVED***"
-export FV_ARM_CLIENT_SECRET="***REMOVED***"
-export FV_ARM_TENANT_ID="***REMOVED***"
-export FV_ARM_ENVIRONMENT="public"
-
-eval "$(ntfy shell-integration)"
-AUTO_NTFY_DONE_IGNORE="vim nano screen tmux man mpv nmtui"
+      eval "$(ntfy shell-integration)"
+      AUTO_NTFY_DONE_IGNORE="vim nano screen tmux man mpv nmtui"
     '';
   };
 
