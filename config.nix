b8c6036c41205c9b171825bf040ee037f0d5cf0e
Daniel Løvbrøtte Olsen <daniel@dodsorf.as>
2020-05-12 13:30:16 +0200

    Use nix channels for nur and dan

--- config.nix
+++ config.nix
@@ -1,19 +1,18 @@
 let
   unstable = import <nixos-unstable> { }; 
   stable = import <nixos> {};
+  nur = import <nur> { pkgs = unstable; };
 in
 {
   allowUnfree = true;
  
   packageOverrides = pkgs: {
     unstable = unstable;
-    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+    nur = nur;
+    dan = import <dan> {
       pkgs = unstable;
     };
-    dan = import (builtins.fetchTarball "https://git.dodsorf.as/Dandellion/NUR/-/archive/master/NUR-master.tar.gz") {
-      pkgs = unstable;
-    };
-    danstable = import (builtins.fetchTarball "https://git.dodsorf.as/Dandellion/NUR/-/archive/master/NUR-master.tar.gz") {
+    danstable = <dan> {
       pkgs = stable;
     };
   };
