51ce1319b11b3ba33b04f51e678a7bfa07b78a2c
Daniel Olsen <daniel.olsen99@gmail.com>
2020-05-20 00:15:32 +0200

    use custom ffmpeg

--- overlays/av1client.nix
+++ overlays/av1client.nix
@@ -14,7 +14,7 @@ self: super:
       sed -i 's,rm,${super.coreutils}/bin/rm,' $out/bin/av1client
       sed -i 's,curl,${super.curl}/bin/curl --cacert ${super.cacert}/etc/ssl/certs/ca-bundle.crt,' $out/bin/av1client
       sed -i 's,jq,${super.jq}/bin/jq,' $out/bin/av1client
-      sed -i 's,ffmpeg ,${super.ffmpeg}/bin/ffmpeg ,' $out/bin/av1client
+      sed -i 's,ffmpeg ,${self.myffmpeg}/bin/ffmpeg ,' $out/bin/av1client
       sed -i 's,aomenc ,${self.mylibaom}/bin/aomenc ,' $out/bin/av1client
 
       chmod +x $out/bin/av1client
