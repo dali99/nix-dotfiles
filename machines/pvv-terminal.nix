1f5c793d772b1d6559cedbc607a142c5a1936de9
Daniel Olsen <daniel.olsen99@gmail.com>
2022-04-24 00:26:26 +0200

    add pvv terminal machine

---
+++ machines/pvv-terminal.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [ ../profiles ];
+
+  machine = {
+    name = "PVV Terminal";
+    eth = null;
+    wlan = null;
+  };
+  profiles.base.enable = true;
+  profiles.gui.enable = true;
+  profiles.non-nixos.enable = true;
+  profiles.xsession.enable = true;
+  profiles.zsh.enable = true;
+
+  services.gammastep =  {
+    enable = true;
+    dawnTime = "7:00-8:15";
+    duskTime = "21:30-22:30";
+  };
+
+  profiles.games.enable = false;
+
+  programs.home-manager = {
+    enable = true;
+    path = "https://github.com/rycee/home-manager/archive/release-21.11.tar.gz";
+  };
+}
