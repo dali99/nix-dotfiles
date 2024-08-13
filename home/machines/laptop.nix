6f5c1073017bb56fe1f6528fdcb4a1f1537af09d
Daniel Olsen <daniel.olsen99@gmail.com>
2024-08-13 02:17:31 +0200

    enable activitywatch

--- home/machines/laptop.nix
+++ home/machines/laptop.nix
@@ -24,5 +24,25 @@
     duskTime = "21:30-22:30";
   };
 
+  services.activitywatch = {
+    enable = true;
+    watchers = {
+      aw-watcher-afk = {
+        package = pkgs.activitywatch;
+        settings = {
+          timeout = 300;
+          poll_time = 2;
+        };
+      };
+      aw-watcher-window = {
+        package = pkgs.activitywatch;
+        settings = {
+          poll_time = 1;
+          # exclude_title = true;
+        };
+      };
+    };
+  };
+
   profiles.games.enable = true;
 }
